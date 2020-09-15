e90163a8e8523e985466e95cdd6de17a7c6edb1d
Dominik Marchewicz <marchewiczd@gmail.com>
2020-09-15 12:44:49 +0200

    New LiteDb class

---
+++ IoT_Api/Database/LiteDb.cs
@@ -0,0 +1,46 @@
+﻿using LiteDB;
+
+namespace IoDataType_Api.Database
+{
+    public class LiteDb<DataType>
+    {
+        private string _connectionString;
+
+        public LiteDb(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+
+        public void Insert(DataType data)
+        {
+            using (var db = new LiteDatabase(_connectionString))
+            {
+                db.GetCollection<DataType>().Insert(data);
+            }
+        }
+
+        public void Delete(ulong id)
+        {
+            using (var db = new LiteDatabase(_connectionString))
+            {
+                db.GetCollection<DataType>().Delete(id);
+            }
+        }
+
+        public DataType SelectById(ulong id)
+        {
+            using (var db = new LiteDatabase(_connectionString))
+            {
+                return db.GetCollection<DataType>().FindById(id);
+            }
+        }
+
+        public void UpdateData(DataType newData)
+        {
+            using (var db = new LiteDatabase(_connectionString))
+            {
+                db.GetCollection<DataType>().Update(newData);
+            }
+        }
+    }
+}
